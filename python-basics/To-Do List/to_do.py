0ab09cfeb770e344b6855c4d60e780688e7578d4
Sachin Karki <itssachin079@gmail.com>
2024-12-01 19:52:36 +0545

    To Do list build

---
+++ python-basics/To-Do List/to_do.py
@@ -0,0 +1,27 @@
+to_do_list=[]
+def todo():
+    user_input=int(input("Press 1 to add, 2 to view , 3 to remove the todo list and 4 to exit\n"))
+    if(user_input==1):
+        activity=input("Enter what you want to do?\n")
+        to_do_list.append(activity)
+        todo()
+    elif(user_input==2):
+        for key,plan in enumerate(to_do_list):
+            print(f"{key+1}. {plan}")
+        todo()
+    elif(user_input==3):
+        index=int(input("Enter the index of the activity you want to remove\n"))
+        remove(index)
+        todo()
+    elif(user_input==4):
+        exit
+    else:
+        print("Enter valid numbers\n")
+        todo()
+
+
+def remove(index):
+    to_do_list.pop(index-1)
+
+print("This is a To-do list app")
+todo()
